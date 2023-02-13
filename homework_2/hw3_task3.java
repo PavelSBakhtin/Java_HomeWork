e8c6466e62cd0e279d4ed7b4909a30994c503e17
PavelSB <1987PavelS@mail.ru>
2023-02-13 18:16:34 +0300

    home work 2 task 3 done

---
+++ homework_2/hw3_task3.java
@@ -0,0 +1,25 @@
+// Напишите метод, который принимает на вход строку (String) и определяет
+// является ли строка палиндромом (возвращает boolean значение)
+
+package homework_2;
+
+import java.util.Scanner;
+
+public class hw3_task3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter some text: ");
+        String input = scan.nextLine();
+        scan.close();
+        boolean res = isPal(input);
+        System.out.println(res);
+    }
+    public static Boolean isPal(String s) {
+        for (int i = 0; i < s.length() / 2; ++i) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
