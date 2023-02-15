008204fb5dd8c40164f01da05c8f5827629a1bad
PavelSB <1987PavelS@mail.ru>
2023-02-16 01:13:12 +0300

    home work 2 task 2 done

--- homework_2/hw2_task2.java
+++ homework_2/hw2_task2.java
@@ -17,13 +17,34 @@ import java.io.BufferedReader;
 
 public class hw2_task2 {
     public static void main(String[] args) throws Exception {
-        FileReader frd = new FileReader("homework_2/hw2_task2_file.txt");
-        BufferedReader brd = new BufferedReader(frd);
-        String line = brd.readLine();
-        StringBuilder str = new StringBuilder();
-        while (line != null) {
-            System.out.println(line);
-            line = brd.readLine();
+        String[] arrDB = FileReadLine("homework_2/hw2_task2_file.json");
+        for (int i = 0; i < arrDB.length; i++) {
+            System.out.println(PrintLine(arrDB[i]));
         }
+    }
+
+    public static String[] FileReadLine(String file) throws Exception {
+        BufferedReader buf = new BufferedReader(new FileReader(file));
+        String str = buf.readLine();
+        String[] listDB = str.split("},\\{");
+        buf.close();
+        return listDB;
+    }
+
+    public static StringBuilder PrintLine(String line) {
+        String nl1 = line.replace("{", "");
+        String nl2 = nl1.replace("}", "");
+        String nl3 = nl2.replaceAll("\"", "");
+        String nl4 = nl3.replace("[", "");
+        String nl5 = nl4.replace("]", "");
+        StringBuilder res = new StringBuilder("");
+        String [] arrDB = nl5.split(",");
+        String [] listNB = {"Студент ", " получил ", " по предмету "};
+        for (int i =0; i < arrDB.length; i++) {
+            String[] arrNDB = arrDB[i].split(":");
+            res.append(listNB[i]);
+            res.append(arrNDB[1]);
+            }
+        return res;
     }
 }
