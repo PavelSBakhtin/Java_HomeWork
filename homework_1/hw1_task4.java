527f349311a0929ad272d852315066601985604f
PavelSB <1987PavelS@mail.ru>
2023-02-16 00:26:09 +0300

    home work 1 task 4 changed

--- homework_1/hw1_task4.java
+++ homework_1/hw1_task4.java
@@ -9,17 +9,38 @@ import java.util.Scanner;
 
 public class hw1_task4 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the equation: ");
-        String input = scan.nextLine();
-        String[] str = input.split(" ");
-        for (int i = 0; i < input.length(); i++) {
-            if (str[i] == "?") {
-                
+        Scanner scan = new Scanner(System.in);  // создаём сканер
+        System.out.print("Enter the equation: "); // просим ввести уравнение
+        String input = scan.nextLine(); // записываем в input уравнение
+        String str = input.replace(" ", ""); // записываем в str уравнение без пробелов
+        int index = str.indexOf("+"); // определяем + или - в уравнении
+        String[] digits = str.split("\\+|-|="); // создаём список чисел в строковом значении
+        scan.close(); // закрываем сканер
+        int aLen = digits[0].length(); // определяем длину числа А
+        int aCount = Count(digits[0]); // определяем количество ? в числе А
+        int bLen = digits[1].length(); // определяем длину числа В
+        int bCount = Count(digits[1]); // определяем количество ? в числе В
+        Integer[] nums = new Integer[3]; // создаём список чисел в числовом значении
+        nums[2] = Integer.parseInt(digits[2]); // записываем в список последним результат уравнения
+        
+
+        /* for (int i = 0; i < 2; i++) {
+            if (digits[i].contains("?")) {
+                nums[i] = Integer.parseInt(digits[i].replaceAll("?", "0"));
+            }
+            else {
+                nums[i] = Integer.parseInt(digits[i]);
+            }
+        } */
+    }
+
+    public static Integer Count(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '?') {
+                count++;
             }
         }
-        scan.close();
+        return count;
     }
 }
-
-// Задачу обязательно решу, пока не додумался как...
